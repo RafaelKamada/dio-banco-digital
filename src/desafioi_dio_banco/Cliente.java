65fb76c864183f5a00f4a4153fb7f8ed3c5d9d96
RafaelKamada <rafael-kamada2007@hotmail.com>
2023-01-10 21:15:41 -0300

    Adicionado listagem de clientes

--- src/desafioi_dio_banco/Cliente.java
+++ src/desafioi_dio_banco/Cliente.java
@@ -3,6 +3,37 @@ package desafioi_dio_banco;
 public class Cliente {
 
 	private String nome;
+	private int idade;
+	private int telefone;
+	private String endereco;
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) throws Exception {
+		if (idade < 18) {
+			throw new Exception("Não é permitido cadastrar um cliente com menos de 18 anos!");
+		}
+		
+		this.idade = idade;
+	}
+
+	public int getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(int telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
 
 	public String getNome() {
 		return nome;
--- src/desafioi_dio_banco/Main.java
+++ src/desafioi_dio_banco/Main.java
@@ -3,27 +3,41 @@ package desafioi_dio_banco;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.swing.JOptionPane;
+
 public class Main {
 
 	public static void main(String[] args) {
-		Banco banco = new Banco();
-		Cliente rafael = new Cliente();
-		rafael.setNome("Rafael");
 		
-		Cliente paulo = new Cliente();
-		paulo.setNome("Paulo");
+		try {
+			Cliente rafael = new Cliente();
+			rafael.setNome("Rafael");
+			rafael.setTelefone(123456789);
+			rafael.setEndereco("Rua Teste, 12");
+			rafael.setIdade(14);
 		
-		Conta cc = new ContaCorrente(rafael);
-		Conta poupanca = new ContaPoupanca(paulo);
-		List<Conta> contas = Arrays.asList(cc, poupanca);
-		banco.setContas(contas);
-		
-		cc.depositar(100);
-		cc.transferir(poupanca, 100);
-		
-		cc.imprimirExtrato();
-		poupanca.imprimirExtrato();
-		
-		banco.listarClientes();
+			Cliente paulo = new Cliente();
+			paulo.setNome("Paulo");
+			paulo.setIdade(11);
+			paulo.setTelefone(123456776);
+			paulo.setEndereco("Rua Dois, 2");
+			
+			Banco banco = new Banco();
+			Conta cc = new ContaCorrente(rafael);
+			Conta poupanca = new ContaPoupanca(paulo);
+			List<Conta> contas = Arrays.asList(cc, poupanca);
+			banco.setContas(contas);
+			
+			cc.depositar(100);
+			cc.transferir(poupanca, 100);
+			
+			cc.imprimirExtrato();
+			poupanca.imprimirExtrato();
+			
+			banco.listarClientes();
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, e.getMessage());
+			e.printStackTrace();
+		}
 	}
 }
